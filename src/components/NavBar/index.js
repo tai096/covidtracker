fb63340c8bbd704a826bf95c5877f59df7a6deb9
tai096 <trinhtaii09@gmail.com>
2022-08-10 22:44:16 +0700

    add animation for NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import logo from "../../assets/img/Logo.png";
 
 export default function NavBar() {
@@ -25,13 +26,14 @@ export default function NavBar() {
   const turnOnMenu = () => {
     setMenuMobie(!menuMobie);
   };
+
   return (
     <>
       <div className="py-7 px-9 flex items-center justify-between">
         <img src={logo} className="w-32 sm:w-52" />
         {windowDimenion.winWidth < 769 ? (
-          <button
-            className="z-10 hover:bg-slate-200 rounded-full p-3 "
+          <motion.button
+            className="z-10 hover:bg-slate-200 transition duration-300 ease-in-out rounded-full p-3 "
             onClick={turnOnMenu}
           >
             <svg
@@ -43,7 +45,7 @@ export default function NavBar() {
             >
               <path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z" />
             </svg>
-          </button>
+          </motion.button>
         ) : (
           <div>
             <Link
@@ -62,18 +64,29 @@ export default function NavBar() {
               to="/tracker"
               className="text-green-900 font-semibold text-lg transition duration-300 ease-in-out hover:text-rose-400 cursor-pointer"
             >
-              Số liệu Thống Kê
+              Số Liệu Thống Kê
             </Link>
           </div>
         )}
       </div>
       {menuMobie ? (
         <>
-          <div
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
             onClick={turnOnMenu}
             className="bg-black fixed top-0 right-0 left-0 bottom-0 bg-opacity-50"
-          ></div>
-          <div className="absolute w-3/5 top-0 bottom-0 left-0 bg-rose-200 bg-opacity-90 flex flex-col text-center">
+          ></motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, x: "-100%" }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: "-100%" }}
+            transition={{ duration: 0.8 }}
+            className="absolute w-3/5 top-0 bottom-0 left-0 bg-rose-200 bg-opacity-90 flex flex-col text-center"
+          >
             <Link
               to="/"
               className="text-green-800  font-semibold text-lg transition duration-500 ease-in-out hover:text-white  hover:bg-rose-500 py-10 cursor-pointer"
@@ -92,7 +105,7 @@ export default function NavBar() {
             >
               Số Liệu Thống Kê
             </Link>
-          </div>
+          </motion.div>
         </>
       ) : (
         <></>
